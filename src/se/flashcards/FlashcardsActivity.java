2b23e2efc4ef7db8012aae32aa175377cb0ea1f6
Hugo S <hugo.flugsvamp@gmail.com>
2012-06-18 02:27:29 +0200

    Setup some library dependencies (ActionBarSherlock and android-support-v4) and made a temporary list for testing

--- src/se/flashcards/FlashcardsActivity.java
+++ src/se/flashcards/FlashcardsActivity.java
@@ -1,13 +1,17 @@
 package se.flashcards;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class FlashcardsActivity extends Activity {
-    /** Called when the activity is first created. */
+public class FlashcardsActivity extends ListActivity {
+
+	static final String[] STUFF = new String[] { "Hipster", "Hopster", "Hapster" };
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.onCreate(savedInstanceState);   
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, STUFF));
     }
 }
