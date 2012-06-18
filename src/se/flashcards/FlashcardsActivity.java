b4bf7addd0351237728125e62f3e9feadc1fb178
Hugo S <hugo.flugsvamp@gmail.com>
2012-06-18 20:03:17 +0200

    Added the ability to add stuff to the list

--- src/se/flashcards/FlashcardsActivity.java
+++ src/se/flashcards/FlashcardsActivity.java
@@ -1,27 +1,44 @@
 package se.flashcards;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockListActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class FlashcardsActivity extends SherlockListActivity {
+	
+	private static final int DIALOG_MAKE_NEW = 0;
 
-	static final String[] STUFF = new String[] { "Hipster", "Hopster", "Hapster" };
+	private ArrayAdapter<String> stuff;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);   
+        
+        stuff = new ArrayAdapter<String>(this, R.layout.list_item, new ArrayList<String>());
         setTheme(R.style.Theme_Sherlock);
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, STUFF));
+        
+        setListAdapter(stuff);
+        
+        stuff.add("Hipster");
+        stuff.add("Hopster");
+        stuff.add("Hapster");
         
         ActionBar bar = getSupportActionBar();
         bar.setTitle("RAYBAN");
@@ -35,12 +52,12 @@ public class FlashcardsActivity extends SherlockListActivity {
         return true;
     }
     
-    @Override
+    @SuppressWarnings("deprecation")
+	@Override
     public boolean onOptionsItemSelected(MenuItem item) {
     	switch(item.getItemId()) {
     		case R.id.menu_save:
-    			Toast toast = Toast.makeText(getBaseContext(), "Saved!", Toast.LENGTH_SHORT);
-    			toast.show();
+    			showDialog(DIALOG_MAKE_NEW);
     			break;
     	}
         return true;
@@ -60,6 +77,33 @@ public class FlashcardsActivity extends SherlockListActivity {
     			Toast toast3 = Toast.makeText(getBaseContext(), "Hap!", Toast.LENGTH_SHORT);
     			toast3.show();
     			break;
+    	}
+    }
+    
+    protected Dialog onCreateDialog(int id) {
+    	switch (id) {
+    		case DIALOG_MAKE_NEW:
+                LayoutInflater factory = LayoutInflater.from(this);
+                final View textEntryView = factory.inflate(R.layout.make_new_dialog, null);
+                final TextView textView = (TextView)textEntryView.findViewById(R.id.text);
+                return new AlertDialog.Builder(this)
+                    .setIconAttribute(android.R.attr.alertDialogIcon)
+                    .setTitle("Make new entry")
+                    .setView(textEntryView)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                        	String text = textView.getText().toString();
+                        	stuff.add(text);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+
+                        }
+                    })
+                    .create();
+    		default:
+    			return null;
     	}
     }
 }
