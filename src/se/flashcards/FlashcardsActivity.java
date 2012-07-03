cf95aa5b2209c9b85994909e25c20f5b9cf85c6c
Hugo S <hugo.flugsvamp@gmail.com>
2012-07-03 04:13:48 +0200

    Added an unfinished ability to remove cardlists from the list

--- src/se/flashcards/FlashcardsActivity.java
+++ src/se/flashcards/FlashcardsActivity.java
@@ -2,12 +2,15 @@ package se.flashcards;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockListActivity;
+//import com.actionbarsherlock.view.ActionMode;
+//import com.actionbarsherlock.view.ActionMode.Callback;
 import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuInflater;
+//import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 
 import android.app.AlertDialog;
@@ -16,9 +19,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.AbsListView.MultiChoiceModeListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -46,11 +56,87 @@ public class FlashcardsActivity extends SherlockListActivity {
         ActionBar bar = getSupportActionBar();
         bar.setTitle("RAYBAN");
         bar.setSubtitle("Only $999");
+        
+        //3.0+ ONLY code!!!
+        final ListView listView = getListView();
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL); //_MODAL
+        listView.setMultiChoiceModeListener(new MultiChoiceModeListener() {
+			@Override
+			public boolean onActionItemClicked(ActionMode mode, android.view.MenuItem item) {
+				switch (item.getItemId()) {
+	            case R.id.menu_delete:
+//	                SparseBooleanArray checkedItems = listView.getCheckedItemPositions();
+//	                for (int i = 0; i < checkedItems.size(); i++) {
+//	                }
+	                mode.finish(); // Action picked, so close the CAB
+	                return true;
+	            default:
+	                return false;
+	        }
+			}
+
+			@Override
+			public boolean onCreateActionMode(ActionMode mode, android.view.Menu menu) {
+				MenuInflater inflater = mode.getMenuInflater();
+				inflater.inflate(R.layout.list_item_longpress_menu, menu);
+				return true;
+			}
+
+			@Override
+			public void onDestroyActionMode(ActionMode mode) {
+
+				
+			}
+
+			@Override
+			public boolean onPrepareActionMode(ActionMode mode, android.view.Menu menu) {
+				return false;
+			}
+
+			@Override
+			public void onItemCheckedStateChanged(ActionMode mode, int pos, long id, boolean checked) {
+				//temp
+				Toast toast = Toast.makeText(FlashcardsActivity.this, "" + listView.getCheckedItemCount(), Toast.LENGTH_SHORT);
+				toast.show();
+			}
+        });
+       
+        
+//        final ListView listView = getListView();
+//        listView.setOnItemLongClickListener(new ListView.OnItemLongClickListener() {
+//			@Override
+//			public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+//				FlashcardsActivity.this.startActionMode(new ActionMode.Callback() {
+//					@Override
+//					public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+//						MenuInflater inflater = mode.getMenuInflater();
+//						inflater.inflate(R.layout.list_item_longpress_menu, menu);
+//						return true;
+//					}
+//
+//					@Override
+//					public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+//						return false;
+//					}
+//
+//					@Override
+//					public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+//						return true;
+//					}
+//
+//					@Override
+//					public void onDestroyActionMode(ActionMode mode) {
+//					}
+//				});
+//				view.setSelected(true);
+//				return true;
+//			}
+//		});
     }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getSupportMenuInflater();
+        com.actionbarsherlock.view.MenuInflater inflater = getSupportMenuInflater();
         inflater.inflate(R.layout.actionbar_menu, menu);
         return true;
     }
