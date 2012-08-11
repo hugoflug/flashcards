7fe66580f879ab3974ede85a6351ab8867cb9c14
Hugo S <hugo.flugsvamp@gmail.com>
2012-08-12 01:55:42 +0200

    Fixed last commit

---
+++ src/se/flashcards/ConfirmDialogFragment.java
@@ -0,0 +1,64 @@
+package se.flashcards;
+
+import se.flashcards.WriteTextDialogFragment.OnTextMadeListener;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager.LayoutParams;
+import android.widget.EditText;
+
+import com.actionbarsherlock.app.SherlockDialogFragment;
+
+public class ConfirmDialogFragment extends SherlockDialogFragment {
+	//containing fragments or activities must implement this
+	public interface OnConfirmedListener {
+		public void onConfirmed();
+	}
+	
+	public static ConfirmDialogFragment newInstance(String title, String question, String confirmText, String dismissText) {
+		ConfirmDialogFragment fragment = new ConfirmDialogFragment();
+		Bundle args = new Bundle();
+		args.putString("title", title);
+		args.putString("question", question);
+		args.putString("confirm", confirmText);
+		args.putString("dismiss", dismissText);
+		fragment.setArguments(args);
+		return fragment;
+		
+	}
+	
+	@Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+		String title = getArguments().getString("title");
+		String question = getArguments().getString("question");
+		String confirm = getArguments().getString("confirm");
+		String dismiss = getArguments().getString("dismiss");
+
+        Dialog dialog = new AlertDialog.Builder(getActivity())
+            .setIconAttribute(android.R.attr.alertDialogIcon)
+            .setTitle(title)
+            .setMessage(question)
+            .setPositiveButton(confirm, new DialogInterface.OnClickListener() {
+            	@Override
+                public void onClick(DialogInterface dialog, int whichButton) {
+                	Fragment target = getTargetFragment();
+                	if (target != null) {
+                		((OnConfirmedListener)target).onConfirmed();
+                	} else {
+                		((OnConfirmedListener)getActivity()).onConfirmed();
+                	}
+                }
+            })
+            .setNegativeButton(dismiss, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                	
+                }
+            })
+            .create();
+        return dialog;
+    }
+}
