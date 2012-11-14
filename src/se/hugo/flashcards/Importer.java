30442fdf14cc13de5238854c4e947e19eb7c6c44
Hugo S <hugo.flugsvamp@gmail.com>
2012-11-14 22:01:30 +0100

    Progress made on exporting CSV

--- src/se/hugo/flashcards/Importer.java
+++ src/se/hugo/flashcards/Importer.java
@@ -7,6 +7,10 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 import au.com.bytecode.opencsv.CSVReader;
 
 public class Importer {
@@ -30,5 +34,33 @@ public class Importer {
 			cardList.add(new Card(new CardContent(s[0]), new CardContent(s[1])));
 		}
 		return cardList;
+	}
+	
+	public static void exportAsCSV(Context context, long listId) throws IOException {
+		InfoSaver saver = InfoSaver.getInfoSaver(context);
+		List<Card> cards = saver.getCards(context, listId, null); //TODO: not null later?
+		String name = saver.nameFromId(listId);
+		exportCards(name, cards);
+	}
+	
+	public static void exportCards(String name, List<Card> cards) {
+		Intent shareIntent = new Intent();
+		
+		StringBuilder csvBuilder = new StringBuilder();
+		
+		ArrayList<Uri> uris = new ArrayList<Uri>();
+		for (Card card : cards) {
+			String question = card.getQuestion().getUriOrString();
+			String answer = card.getAnswer().getUriOrString();
+			csvBuilder.append(question + "," + answer + "\n");
+		}
+		
+		Log.v("flashcards", "csv_out: " + csvBuilder.toString()); //TEMP
+		
+		//TODO: send cards as image content
+		
+		shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+		shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+		shareIntent.setType("*/*");
 	}
 }
