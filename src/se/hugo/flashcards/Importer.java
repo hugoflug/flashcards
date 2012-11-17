53a468a3131720257c0aee2b8bac8b55c9f3f793
Hugo S <hugo.flugsvamp@gmail.com>
2012-11-17 17:49:19 +0100

    Images can now be loaded from CSV

--- src/se/hugo/flashcards/Importer.java
+++ src/se/hugo/flashcards/Importer.java
@@ -1,11 +1,14 @@
 package se.hugo.flashcards;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import android.content.Context;
 import android.content.Intent;
@@ -22,18 +25,47 @@ public class Importer {
 		return cardList;
 	}
 	
-	public static List<Card> importCards(String filename) throws IOException {	
+	public static List<Card> importCards(String filename, BitmapDownsampler sampler) throws IOException {
+		Log.d("flashcards", "filename: " + filename);
+		
 		CSVReader reader = new CSVReader(new FileReader(filename));
 		List<String[]> lines = reader.readAll();
+		reader.close();
 		
 		ArrayList<Card> cardList = new ArrayList<Card>(lines.size());
 		for (String[] s : lines) {
 			if (s.length < 2) {
 				throw new IOException();
 			}
-			cardList.add(new Card(new CardContent(s[0]), new CardContent(s[1])));
+			
+			String filepath = removeLastSegment(filename);
+			
+			CardContent c1, c2;
+			if (Util.isImageFile(s[0])) {
+				Uri imageUri = Uri.parse("file://" + filepath + s[0]);
+				
+				c1 = new CardContent(sampler.decode(imageUri), imageUri);
+			} else {
+				c1 = new CardContent(s[0]);
+			}
+			
+			if (Util.isImageFile(s[1])) {
+				Uri imageUri = Uri.parse("file://" + filepath + s[1]);
+				c2 = new CardContent(sampler.decode(imageUri), imageUri);
+			} else {
+				c2 = new CardContent(s[1]);
+			}
+			
+			cardList.add(new Card(c1, c2));
 		}
+		
 		return cardList;
+	}
+	
+	private static String removeLastSegment(String filepath) {
+		Pattern p = Pattern.compile("(^.*/).*");
+		Matcher m = p.matcher(filepath); 
+		return m.replaceAll("$1");
 	}
 	
 	public static void exportAsCSV(Context context, long listId) throws IOException {
