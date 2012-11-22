eb75c5b9c5f86b24421f1943e69dd379119e209c
Hugo S <hugo.flugsvamp@gmail.com>
2012-11-22 21:07:09 +0100

    Made PreferencesActivity ABS-styled on <3.0 and added a back button to the Settings

--- src/se/hugo/flashcards/SettingsActivity.java
+++ src/se/hugo/flashcards/SettingsActivity.java
@@ -1,14 +1,38 @@
 package se.hugo.flashcards;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import com.actionbarsherlock.view.MenuItem;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends SherlockPreferenceActivity {
 	public static final String AUTO_SHUFFLE = "auto_shuffle";
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        setTheme(R.style.Theme_Sherlock);
+        ActionBar bar = getSupportActionBar();
+        Util.customizeActionBar(getResources(), bar);     
+        bar.setDisplayHomeAsUpEnabled(true);
+        
         addPreferencesFromResource(R.xml.preferences);
     }
+    
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch(item.getItemId()) {
+    		case android.R.id.home:
+    	    	Intent intent = new Intent(this, FlashcardsActivity.class);
+    	    	intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    	    	intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+    	    	startActivity(intent);    			
+    		break;
+    	}
+    	return true;
+	}
 }
